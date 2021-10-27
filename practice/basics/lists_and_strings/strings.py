8d801c558e6f3f72163bf10e9036ebda63ba17f5
Daniil <dan.io.oreshnikov@gmail.com>
2021-10-27 05:15:22 +0300

    Updated statements

--- practice/basics/lists_and_strings/strings.py
+++ practice/basics/lists_and_strings/strings.py
@@ -1,20 +1,22 @@
 def reverse(s):
-    """Вернуть исходную строку, записанную наоборот ('abc' -> 'bca')"""
+    """
+    Вернуть исходную строку, записанную наоборот
+    reverse('abc') -> 'cba'
+    """
     pass
 
 
 def swap_case(s):
     """
-    Вернуть исходную строку, но нижний регистр заменён на верхний, а верхний на нижний.
-    Не использовать стандартную функцию str.swapcase()
-    ('Hello world' -> 'hELLO WORLD')
+    Заменить регистр каждого символа, входящего в строку, на противоположный
+    swap_case('Hello world') -> 'hELLO WORLD'
     """
     pass
 
 
 def censor_words(s, blacklist):
     """
-    Вернуть исходную строку, но подстроки из списка blacklist заменить на звёздочки (*) такой же длины.
+    Вернуть исходную строку, в которой слова из blacklist заменены на звездочки
     censor_words('hello world', ['hello']) -> '***** world')
     """
     pass
@@ -22,17 +24,17 @@ def censor_words(s, blacklist):
 
 def remove_duplicates(s):
     """
-    Нужно удалить идущие подряд повторы слов в строке. Строка состоит только из букв нижнего регистра и пробелов.
-    'hello nice nice world hello' -> 'hello nice world hello'
+    Удалить идущие подряд повторы слов в строке
+    remove_duplicates('hello nice nice world hello') -> 'hello nice world hello'
     """
     pass
 
 
 def parentheses(s):
     """
-    Проверить, закрыты ли все пары круглых скобок в строке.
-    '(abc(1+2))' -> True
-    '(lalala' -> False -- скобка не закрыта
-    '(hello)world)' -> False -- слишком много закрывающих скобок
+    Проверить, закрыты ли все пары круглых скобок в строке
+    parentheses( '(abc(1+2))' )    -> True
+    parentheses( '(lalala' )       -> False - скобка не закрыта
+    parentheses( '(hello)world)' ) -> False - слишком много закрывающих скобок
     """
     pass
